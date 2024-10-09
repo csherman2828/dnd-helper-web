2da9ff04a7ba30b1e042298901423bb2205c9ec3
Chris Sherman <csherman2828@gmail.com>
2024-10-08 21:59:19 -0400

    set up basic page layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <v-layout>
+    <v-app-bar>
+      <v-toolbar-title>Shermaniac VTT</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-layout>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import './assets/base.css';
-
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
+import vuetify from './vuetify';
+import pinia from './pinia';
 
 const app = createApp(App);
-
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
-
+app.use(vuetify);
 app.mount('#app');
